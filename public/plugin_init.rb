171ffc280e485ebe2cf14146e7d58c78f8fcd1c2
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2023-12-11 12:27:46 -0500

    typo

--- public/plugin_init.rb
+++ public/plugin_init.rb
@@ -50,7 +50,7 @@ Rails.application.config.before_initialize do
             puts "\n\nAspace Custom Locales Plugin:
             WARNING - Public localization files for the selected default language do not exist.
             Please ensure that you have localization files named: #{AppConfig[:aspace_custom_localizations_public_default].to_s}.yml 
-            in public/locales, common/locales, and common/locales/enums and of this plugin.\n\n"
+            in public/locales, common/locales, and common/locales/enums of this plugin.\n\n"
           end
 
         end
