2b1b1bd797e2c5e8abf7bc6749f446805fff98c4
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2024-06-27 12:01:26 -0400

    remove commented out line

--- lib/aspace_custom_locales_helper.rb
+++ lib/aspace_custom_locales_helper.rb
@@ -53,7 +53,6 @@
       if app_name == 'backend'
         I18n::Backend::Simple.include(I18n::Backend::Fallbacks)
         I18n.fallbacks = I18n::Locale::Fallbacks.new(de: :en, es: :en, fr: :en, ja: :en, localization.to_s => :en)
-        #I18n.fallbacks = [AppConfig[:locale]]
       else
         config.i18n.fallbacks = [AppConfig[:locale]]
       end
